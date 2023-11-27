629f8c4d5c43d9ec199292afc6d69521c44df2db
Nero <14k21096@gmail.com>
2023-11-27 19:02:12 +0300

    update

---
+++ task8/sql.py
@@ -0,0 +1,15 @@
+create_table = """
+CREATE TABLE credit_cards_numbers( id PRIMARY KEY, name varchar(50) NOT NULL, speciality varchar(50) NOT NULL);
+"""
+
+insert_to_table1 = """
+INSERT INTO credit_cards_numbers (name, speciality) VALUES ('Oleg', 'somelue');
+"""
+
+insert_to_table2 = """
+INSERT INTO credit_cards_numbers (name, speciality) VALUES ('Vasya', 'python');
+"""
+
+insert_to_table3 = """
+INSERT INTO credit_cards_numbers (name, speciality) VALUES ('Lena', 'ux');
+"""
