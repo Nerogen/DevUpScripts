13688b82053bdc3966fcfc7e7bf1b68fc529eac9
Nero <14k21096@gmail.com>
2023-11-28 21:31:05 +0300

    update

--- task8/sql.py
+++ task8/sql.py
@@ -5,3 +5,25 @@ CREATE TABLE credit_cards_numbers( record_id INT NOT NULL PRIMARY KEY, name varc
 
 INSERT INTO credit_cards_numbers (record_id, name, speciality) VALUES (1, 'Oleg', 'somelue'), (2, 'Vasya', 'python'), (3, 'Lena', 'ux');
 """
+
+"""
+CREATE TABLE credit_cards_numbers (
+  id SERIAL PRIMARY KEY,
+  card_number VARCHAR(16)
+);
+"""
+
+"""
+INSERT INTO credit_cards_numbers (card_number) VALUES
+  ('1234567890123456'),
+  ('9876543210987654'),
+  ('1111222233334444');
+"""
+
+"""
+SELECT * FROM credit_cards_numbers;
+"""
+
+"""
+DROP DATABASE jundb;
+"""
