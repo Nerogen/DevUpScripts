d9f2f8bc52586fcbfb1787988e7a3084676d7c7a
Nero <14k21096@gmail.com>
2024-02-14 12:26:28 +0300

    update

--- task27/metrics.py
+++ task27/metrics.py
@@ -1,4 +1,4 @@
 """
-100 * (1 - (irate(node_cpu_seconds_total{cpu="0", mode="idle"}[1m]))) > 80
-(100 * ((node_memory_MemTotal_bytes{instance="172.31.38.7:9100"} - node_memory_MemFree_bytes{instance="172.31.38.7:9100"} - node_memory_Buffers_bytes{instance="172.31.38.7:9100"} - node_memory_Cached_bytes{instance="172.31.38.7:9100"} - node_memory_SReclaimable_bytes{instance="172.31.38.7:9100"}) / node_memory_MemTotal_bytes{instance="172.31.38.7:9100"})) > 60
+100 * (1 - (irate(node_cpu_seconds_total{cpu="0", mode="idle", instance="172.31.35.104:9100"}[1m]))) > 80
+(100 * ((node_memory_MemTotal_bytes{instance="172.31.47.210:9100"} - node_memory_MemFree_bytes{instance="172.31.47.210:9100"} - node_memory_Buffers_bytes{instance="172.31.47.210:9100"} - node_memory_Cached_bytes{instance="172.31.47.210:9100"} - node_memory_SReclaimable_bytes{instance="172.31.47.210:9100"}) / node_memory_MemTotal_bytes{instance="172.31.47.210:9100"})) > 60
 """
